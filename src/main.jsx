9e3fe49136b14cb115f56e374cc8ac0d89fe969a
Easara Weerasinghe <it21259852@my.sliit.lk>
2024-11-30 14:40:59 +0530

    updated main.jsx for redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import store from "./redux/store.jsx";
+import { ConfigProvider } from "antd";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <ConfigProvider
+      theme={{
+        token: {
+          fontFamily: "Poppins, sans-serif",
+        },
+      }}
+    >
+      <App />
+    </ConfigProvider>
+  </Provider>
+);
